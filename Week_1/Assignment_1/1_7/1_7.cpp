73c74d4c793700443c485a7b713f05dbe9f17cad
Arif Khan <ak5316221@gmail.com>
2025-08-21 22:18:23 +0100

    Question 7

---
+++ Week_1/Assignment_1/1_7/1_7.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    for (int i = 1; i <= x; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << ((j + i) % 2);
+        }
+        cout << endl;
+    }
+}

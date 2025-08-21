96e694eedab9c4103db49c41081749f5905617a1
Arif Khan <ak5316221@gmail.com>
2025-08-21 19:19:39 +0100

    Question 5

---
+++ Week_1/Assignment_1/1_5/1_5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    for (int i = x; i > 0; i--) {
+        int stars = (2*i);
+        int spaces = (x-i+1);
+
+        for (int j = 1; j < spaces; j++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j < stars; j++) {
+            cout << "*";
+        }
+
+        for (int j = 1; j < spaces; j++) {
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}

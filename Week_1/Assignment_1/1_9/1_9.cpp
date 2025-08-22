482b063928c71fdfd8088eeb7c9db4cdd990d2e4
Arif Khan <ak5316221@gmail.com>
2025-08-22 16:31:19 +0100

    Question 9

--- Week_1/Assignment_1/1_9/1_9.cpp
+++ Week_1/Assignment_1/1_9/1_9.cpp
@@ -9,11 +9,15 @@ int main()
     int x;
     cin >> x;
 
-    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    for (int i = 1; i <= x; i++) {
-        for (int j = 1; j <= i; j++) {
-            cout << alphabet[x - i + j - 1];
+    if (x >= 1 && x <= 26) {
+        string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= i; j++) {
+                cout << alphabet[x - i + j - 1];
+            }
+            cout << endl;
         }
-        cout << endl;
+    } else {
+        cout << "Incorrect value, please enter a number between 1 and 26";
     }
 }

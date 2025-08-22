4b0d037432a53ccbfd73918ad92ef9df50dea4de
Arif Khan <ak5316221@gmail.com>
2025-08-22 16:50:04 +0100

    Question 10

--- Week_1/Assignment_1/1_10/1_10.cpp
+++ Week_1/Assignment_1/1_10/1_10.cpp
@@ -9,11 +9,40 @@ int main()
     int x;
     cin >> x;
 
-    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    for (int i = 1; i <= x; i++) {
-        for (int j = 1; j <= i; j++) {
-            cout << alphabet[x - i + j - 1];
-        }
+    for (int i = 1; i <= 2 * x; i++) {
+        if (i <= x) {
+            int stars = (x-i);
+            int spaces = (2*(i-1));
+
+            for (int j = 0; j <= stars; j++) {
+                cout << "*";
+            }
+
+            for (int j = 0; j < spaces; j++) {
+                cout << " ";
+            }
+
+            for (int j = 0; j <= stars; j++) {
+                cout << "*";
+            }
+        } else if (i > x) {
+            int stars = (i - x);
+            int spaces = (2 * (2*x - i));
+
+            //cout << spaces;
+            for (int j = 0; j < stars; j++) {
+                cout << "*";
+            }
+
+            for (int j = 0; j < spaces; j++) {
+                cout << " ";
+            }
+
+            for (int j = 0; j < stars; j++) {
+                cout << "*";
+            }
+            
+            }
         cout << endl;
     }
 }

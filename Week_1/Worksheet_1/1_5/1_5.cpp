eb38a2cdadc57e7362d0df1bb9dba27e40468ae1
Arif Khan <ak5316221@gmail.com>
2025-09-23 11:03:56 +0100

    Q5

---
+++ Week_1/Worksheet_1/1_5/1_5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    if (n % 2 == 0) {
+        cout << "Even" << endl;
+    } else {
+        cout << "Odd" << endl;
+    }
+
+
+
+}
